9d36c641d558b4b48a2faf51da7028f61d49cc49
wangjiabao <840028401@qq.com>
2024-11-07 17:46:43 +0800

    ok

---
+++ internal/model/entity/trader_position.go
@@ -0,0 +1,19 @@
+// =================================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// =================================================================================
+
+package entity
+
+import (
+	"github.com/gogf/gf/v2/os/gtime"
+)
+
+// TraderPosition is the golang structure for table trader_position.
+type TraderPosition struct {
+	Id             uint        `json:"id"             ` //
+	Symbol         string      `json:"symbol"         ` //
+	PositionSide   string      `json:"positionSide"   ` //
+	PositionAmount float64     `json:"positionAmount" ` //
+	CreatedAt      *gtime.Time `json:"createdAt"      ` //
+	UpdatedAt      *gtime.Time `json:"updatedAt"      ` //
+}
